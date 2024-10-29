0a8cbc8111697516d09d90e06656cb9a0c170a4c
Cyrus Binoy <cyrusbinoy123@gmail.com>
2024-10-29 18:44:38 -0400

    initial commit

---
+++ Arrays/max_and_min.cpp
@@ -0,0 +1,25 @@
+// creating a program to find the maximum and the minimum of the array
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {3,2,-2,9,10,-4,7};
+    int arr_size = sizeof(arr)/ sizeof(int);
+    int max = arr[0], min = arr[0]; // comparing the numbers with respect to the array's first element
+    
+    for (int i = 1; i<=arr_size; i++){ // traversing from the second element
+        if (arr[i] < min){
+            min = arr[i];
+        }
+        if (arr[i] > max){
+            max = arr[i];
+
+        }
+    }
+
+    cout << " The minimum and the maximum element is " << min << " and " << max << " respectively";
+
+    
+    return 0;
+}
